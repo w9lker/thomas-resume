3a966d1dc6cbe7ea006101f9d44439c3be9b9097
w9lker <skhalikov2@gmail.com>
2023-11-29 23:35:29 +0800

    Updated Teaching.vue

--- src/components/Teaching.vue
+++ src/components/Teaching.vue
@@ -4,116 +4,195 @@
             <img src="../assets/lecture_hall.jpg" class="" alt="'Lecture Lecture' by cogdogblog is marked with CC0 1.0.">
         </div>
         <div class="wrapper">
+            <div class="header">
+                <div class="header-body">
+                    <h3>Jacob's Teaching Experience and Philosphy</h3>
+                    <p>Between Beijing Foreign Studies University, UCLA, and CUHK, Jacob has taught a diverse range of students in
+                    terms of age, culture, ethnicity, and class in distinct institutional contexts.</p>
+                    <p>My general teaching objectives are to have his students:</p>
+                    <ol>
+                        <li>connect what they learn in class to what they observe in society,</li>
+                        <li> acquire tacit knowledge through regular exercises in research methods, and</li>
+                        <li>cultivate creative life-long habits ofeducating the self and society throughout their personal and professional life. </li>
+                    </ol>
+                    <p>He succeeds in these objectives whenever his students respectively describe observing examples of what he has taught them, become more
+                    confident and resourceful in drawing upon the methods he introduced to them, and teach him or inspire him
+                    with new research ideas.</p>
+                </div>
+            </div>
+            
+        </div>
+        <div class="wrapper">
             <div class="teaching-exp">
                 <div class="experience">
-<p>
-    <h3>Jacob's Teaching Experience and Philosophy</h3>
-                     <li> Between Beijing Foreign Studies University, UCLA, and CUHK, Jacob has taught a diverse range of students in
-terms of age, culture, ethnicity, and class in distinct institutional contexts. </li>
-   
-        <li>  My general teaching objectives are to have his students:</li>
-   <li> 1) connect what they learn in class to what they observe in society, </li>
-    <li>2) acquire tacit knowledge through regular exercises in research methods, and  </li>
-     <li> 3) cultivate creative life-long habits of
-educating the self and society throughout their personal and professional life. </li>
-    <li>He succeeds in these objectives whenever his students respectively describe observing examples of what he has taught them, become more
-confident and resourceful in drawing upon the methods he introduced to them, and teach him or inspire him
-with new research ideas. </li>
-    
-    </p>
                     <h3>Research Assistant Professor</h3>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Spring 2022 and Spring 2023 </p>
-                        <p>Qualitative Research Methods, the Chinese University of Hong Kong Department of 
-                        Sociology</p>
+                    <div class="course-description-body">
+                        <div class="flexbox-entry">
+                            <div class="entry">
+                                <p class="date-endpoints">Spring 2022 and Spring 2023 </p>
+                                <p>Qualitative Research Methods, the Chinese University of Hong Kong Department of 
+                                Sociology</p>
+                            </div>
                         </div>
-                    </div>  
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Fall 2023 </p>
-                        <p>Sociology and Modernity in the Age of Generative Artificial Intelligence, 
-                            the Chinese University of Hong Kong Department of 
-                        Sociology</p>
+                        <div class="side-image space-down">
+                            <img src ="../assets/qualitative_methods.jpg" width="700px">
                         </div>
+                    </div>
+                    <div class="course-description-body">
+                        <div class="flexbox-entry">
+                            <div class="entry">
+                            <p class="date-endpoints">Fall 2023 </p>
+                            <p>Sociology and Modernity in the Age of Generative Artificial Intelligence, 
+                                the Chinese University of Hong Kong Department of 
+                            Sociology</p>
+                            </div>
+                        </div>
+                        <div class="side-image space">
+                            <div class="image-header">
+                                <p>The jobs that Masters students at CUHK most want to have 
+                                after graduating are those most likely to be replaced by GenAI in the future.</p>
+                            </div>
+                            <img src ="../assets/qualtrics.jpg" width="700px">
+                        </div> 
                     </div>
                 </div>
                 <div class="experience">
                     <h3>Instructor at UCLA</h3>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Summer 2020</p>
-                        <p>Law and Society, University of California, Los Angeles, Department of Sociology</p>
+                    <div class="course-description-body">
+                        <div class="flexbox-entry">
+                            <div class="entry">
+                            <p class="date-endpoints">Summer 2020</p>
+                            <p>Law and Society, University of California, Los Angeles, Department of Sociology</p>
+                            </div>
+                        </div>  
+                        <div class="side-image">
+                            <img src="../assets/law_society_2.png" width="700px">
                         </div>
-                    </div>  
-                    <div class="extra-image">
+                    </div>
+                    <!-- <div class="extra-image">
                         <img src="../assets/law_and_soc_img_1.jpg" width="45%">
                         <div class="vertical-line"></div>
                         <img src="../assets/law_society_2.png" width="45%">
-                    </div>
+                    </div> -->
                 </div>
                 <div class="experience">
                     <h3>Teaching Assistant for Undergraduate Students at UCLA</h3>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Spring 2022</p>
-                        <p>Contemporary Sociological Theory, Jeff Guhin, UCLA Department of Sociology</p>
-                        </div>
                     </div>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Fall 2019</p>
-                        <p>Comparative Acculturation and Assimilation for Cecilia Menjivar, UCLA Department of Sociology</p>
+                        <div class="course-description-body">
+                            <div class="flexbox-entry">
+                                <div class="entry">
+                                <p class="date-endpoints">Spring 2022</p>
+                                <p>Contemporary Sociological Theory, Jeff Guhin, UCLA Department of Sociology</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Summer 2019</p>
-                        <p>(History/Chicano Studies), Nina Ponce (Public Health), Asia Pacific Institute and Zhejiang University</p>
+                        <div class="course-description-body">
+                            <div class="flexbox-entry">
+                                <div class="entry">
+                                <p class="date-endpoints">Fall 2019</p>
+                                <p>Comparative Acculturation and Assimilation for Cecilia Menjivar, UCLA Department of Sociology</p>
+                                </div>
+                            </div>
+                            <div class="side-image space-down">
+                                <img src="../assets/microaggression_jpg.jpg" width="700px">
+                            </div>
                         </div>
-                    </div>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Spring 2017</p>
-                        <p>Self and Society for Jack Katz, UCLA Department of Sociology</p>
+                        <div class="course-description-body">
+                            <div class="flexbox-entry">
+                                <div class="entry">
+                                <p class="date-endpoints">Summer 2019</p>
+                                <p>Contemporary American Politics, Society and Stratification for Cesar Ayala (Sociology), Robert
+                                Chao Romero (History/Chicano Studies), and Nina Ponce (Public Health), Asia Pacific Institute and
+                                Zhejiang University, Summer 2019</p>
+                                </div>
+                            </div>
+                            <div class="side-image">
+                                <img src="..\assets\addis_ababa.jpg" width="700px">
+                            </div>
                         </div>
-                    </div>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Winter 2017</p>
-                        <p>Social Norms for Patrick Riley, UCLA Department of Sociology</p>
+                        <div class="course-description-body">
+                            <div class="flexbox-entry">
+                                <div class="entry">
+                                <p class="date-endpoints">Spring 2017</p>
+                                <p>Self and Society for Jack Katz, UCLA Department of Sociology</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Fall 2016 </p>
-                        <p>Introduction to Sociology for Terri Anderson, Department of UCLA Sociology</p>
+                        <div class="course-description-body">
+                            <div class="flexbox-entry">
+                                <div class="entry">
+                                <p class="date-endpoints">Winter 2017</p>
+                                <p>Social Norms for Patrick Riley, UCLA Department of Sociology</p>
+                                </div>
+                            </div>
+                            <div class="side-image">
+                                <img src="..\assets\cultural_appropriation.jpg" width = 700px>
+                            </div>
                         </div>
-                    </div>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Fall 2014</p>
-                        <p>Sociology of Mass Communications for Gabriel Rossman, Department of UCLA Sociology</p>
+                        <div class="course-description-body">
+                            <div class="flexbox-entry">
+                                <div class="entry">
+                                <p class="date-endpoints">Fall 2016 </p>
+                                <p>Introduction to Sociology for Terri Anderson, Department of UCLA Sociology</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                
+                        <div class="course-description-body">
+                            <div class="flexbox-entry">
+                                <div class="entry">
+                                <p class="date-endpoints">Fall 2014</p>
+                                <p>Sociology of Mass Communications for Gabriel Rossman, Department of UCLA Sociology</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>                
                 <div class="experience">
                     <h3>Foreign Expert Instructor for Graduate Students at Beijing Foreign Studies University</h3>
-                    <div class="flexbox-entry">
-                        <div class="entry">
-                        <p class="date-endpoints">Fall 2012 and Spring 2013</p>
-                        <p>Academic and Professional Writing, The Immigrant Cultures of the United States,
-                            European Cultures and Societies
-                        </p>
-                        </div>
-                    </div>   
+                    <div class="course-description-body">
+                        <div class="flexbox-entry">
+                            <div class="entry">
+                            <p class="date-endpoints">Fall 2012 and Spring 2013</p>
+                            <p>Academic and Professional Writing, The Immigrant Cultures of the United States,
+                                European Cultures and Societies
+                            </p>
+                            </div>
+                            <div class="side-image space-down">
+                                <img src="..\assets\critical_assimilation.jpg" width="700px">
+                            </div>
+                        </div>  
+                    </div>
                 </div>
             </div>
-        </div>
     </body>
 </template>
 <style scoped>
+    .image-header{
+        width:700px;
+        font-weight: bold;
+        text-align:center;
+        align-items: center;
+    }
+    .space-down{
+        margin-bottom:100px;
+    }
+    .side-image{
+        margin-left:100px;
+    }
+    .course-description-body{
+        display:flex;
+    }
+    .side-images{
+        justify-content: space-between;
+        margin-left:100px;
+        display:flex;
+        flex-direction: column;
+    }
+    .flexbox-courses{
+        display: flex;
+    }
+    .course-list{
+        max-width:400px;
+    }
     .vertical-line{
         border-left: solid black;
         background-color: black;
@@ -130,7 +209,7 @@ with new research ideas. </li>
     }
     .flexbox-entry{
         display:flex;
-        justify-content: end;
+        justify-content: start;
     }
     .date-endpoints{
         font-weight: bold;
@@ -141,6 +220,9 @@ with new research ideas. </li>
         font-weight: bold;
     }
     .teaching-exp{
+        margin-top:30px;
+    }
+    .header{
         margin-top:100px;
     }
     .header-image{
