738ced2c7a6fcd5b3a728d894baf457c5e0196b0
t12679 <32949438+t12679@users.noreply.github.com>
2023-11-29 19:15:36 +0800

    Update AboutMe.vue

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -12,8 +12,8 @@
                     and writing short stories, poetry and plays. Originally thinking he wanted to become a publisher for many of his 
                     friends in the poetry slam and arts scene, he first majored in business administration at UC Berkeley until 
                     he became more fascinated by changes in the world and switched to an interdisciplinary studies field major, 
-                    specializing in the study of globalization. While at <b>Berkeley</b> he also studied and trained in a variety of 
-                    Asian massage and holistic bodywork therapies at the Acupressure Institute. Since his grandmother presciently invested in 
+                    specializing in the study of globalization. While at <b>Berkeley</b> he also studied and trained in acupressure therapy, 
+                    a variety of Asian massage and holistic bodywork therapies at the Acupressure Institute. Since his grandmother presciently invested in 
                     his education shortly after birth, he was fortunate and privileged to have graduated without debt, so after he graduated 
                     he could spend a year <b>traveling around North America and every country in Europe</b> staying at the homes of locals and especially 
                     the elderly to learn more about each country's history and culture. He returned to the US to complete over 
@@ -24,28 +24,28 @@
 
                     Jacob completed a <b>Masters Degree in Social Sciences at University of Chicago</b> during the 2011-2012 academic year, where he 
                     specialized in studying qualitative methods and political theory and philosophy from Thucydides to John Rawls and contemporary 
-                    political theorists. He spent a gap year studying Mandarin and teaching Chinese graduate students academic writing and European 
-                    and American Culture and History at <b>Beijing Foreign Studies University for one year.</b> 
+                    political theorists. He spent a gap year studying Mandarin and teaching Chinese graduate students academic and professional writing, 
+                    European Cultures and Societies, and the Immigrant Cultures of the United States at <b>Beijing Foreign Studies University for one year.</b> 
 
                     In 2013, Jacob began a <b>Ph.D at UCLA's Department of Sociology</b>, where he launched a major multi-thousand person survey in 
                     Mainland China of three types of non-migrants: 1) those denied non-immigrant (temporary) visas, 2) those deterred from 
                     applying for visas due to social forces and government policy, and 3) those that had planned to immigrate but who become 
                     disenchanted with the immigration dream after arrival and return. In addition to his dissertation, The Denied, the Deterred and 
-                    the Disenchanted: Why a Variety of Potential Emigrants Never Immigrated which he plans to turn into a book, Jacob has published 
-                    articles in Theory and Society, International Journal of Sociology, European Journal of Sociology, International Migration, Contexts, and Law &: the Southern California Interdisciplinary Journal 
-                    of Law. Having taken over <b>85 seminars at UCLA</b> during his entire seven years at UCLA—including some in the Departments of <b>Anthropology, 
+                    the Disenchanted: Why a Variety of Potential Emigrants Never Immigrated. Having taken over <b>85 seminars at UCLA</b> during his entire seven years at UCLA—including some in the Departments of <b>Anthropology, 
                     East Asian Languages and Cultures, Education, English, Film, Geography, Information Studies, Law, Political Science, Psychology, 
                     Public Policy, and Statistics</b>—he has learned to utilized a wide range of methods and theories in his interdisciplinary research. 
                     In addition to serving as a teaching assistant for UCLA courses in Contemporary Sociological Theory, Immigrant Acculturation and 
                     Assimilation, Social Norms, Self and Society, and the Sociology of Mass Communications, he served as UCLA’s online instructor of 
-                    Law and Society for undergraduate students in the US, China, Taiwan, and Pakistan during the 2020 Summer Sessions. 
+                    Law and Society for undergraduate students in the US, China, Taiwan, and Pakistan during the 2020 Summer Sessions. Jacob has published 
+                    several of his past research papers into articles in Theory and Society, International Journal of Sociology, European Journal of Sociology, International Migration, Contexts, and Law &: the Southern California Interdisciplinary Journal 
+                    of Law. 
 
                     After completing his Ph.D in August 2020, he became a Postdoctoral <b>Research Associate</b> for the Center On Contemporary China at 
                     <b>Princeton University and Peking University</b>, researching with the demographers Yu Xie, Lemeng Liang, and Shigeto Sonoda 
                     the sociological impacts of COVID-19 on 
                     human mobility and migration and how public opinion of different nationalities toward the U.S. and China has changed since the 
                     outbreak of the coronavirus pandemic. 
-
+                
                     After a year of excellent methodological training in quantitative methods and preparing papers for publication at 
                     Princeton University, he has taken up a three-year project as a <b>Research Assistant Professor in the Department of Sociology 
                     at the Chinese University of Hong Kong</b> where he has a tentative project that draws on text data from social media and an 
@@ -54,6 +54,14 @@
                     Despite the unprecedented absolute volume of the recent outflow, through such research Jacob seeks to explain the puzzle of why only 2% per year has emigrated from Hong Kongers
                     when 40% of the population has reported in the Hong Kong Institute of Asian Pacific Studies that they would emigrate if they had an opportunity
                     and 15% said they made concrete plans to soon do so.
+
+                     More recently, he has revised and resubmitted to Cambridge University Press a seven-chapter book manuscript, Denial, Deterrence, and 
+                    Disenchantment: Why Many Never Immigrate. The book focuses in detail on how three mechanisms reduce the number of people who eventually 
+                    immigrate and generate different sub-populations of might-have-been-immigrants: 1) the denial of visas, 2) how those who would like 
+                    to apply for visas become deterred from doing so, and 3) how those who aspire to immigrant into a country become disenchanted after 
+                    arriving and give up on immigrating.
+                    
+                  
                 </div>
             </div>
             <div class="about-me-news">
