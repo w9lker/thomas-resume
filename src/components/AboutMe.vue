032d5c4822359f2f01b8e20f2a5b6d47782a5e89
Saidbek Khalikov <skhalikov2@gmail.com>
2024-07-01 13:47:19 +0800

    Added the news entries

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -71,13 +71,209 @@
                     </div>
                 </div>
                 <div class="news-content wrapper">
-                    Coming Soon....
+                    <div>
+                        <h2>2024</h2>
+                        <ul>
+                            <li><b>June 25th</b>: Princeton University's Center on Migration and Development appoints me as an honorary Research Fellow</li>
+
+                            <li><b>June 20th</b>: Presented "What Ties Individuals To a Society?" at Tarleton State University in Stephenville, Texas</li>
+
+                            <li><b>May 27th</b>: Invited Talk to Present "Family Ties as a Deterrent to International Migration" at Corvinus University's  
+                            Department of Sociology and Social Policy  (Online)</li>
+
+                            <li><b>June 14th</b> : Presented ”I Wanted To Immigrate Until ____: What Type of Sudden Shocks End Immigration?” at 
+                            2024 International Chinese Sociological Association Annual Meeting at University of Macau</li>
+
+                            <li><b>June 10th-12th</b>: Debut-presented Push-Retain-Pull-Repel Model at UC Berkeley's Summer Institute on 
+                            Migration Research Methods</li>
+
+                            <li><b>June 7th-10th</b>: Field Research Recruiting Hong Kong Immigrants in San Francisco and Oakland, 
+                            California, USA</li>
+
+                            <li><b>May 28th</b> : Invited Talk to Present "What Ties Individuals To a Society?" at Virginia Union University 
+                            Department of Social Welfare (Online)</li>
+
+                            <li><b>April 20th - June 4th</b>: Field Recruiting Hong Kong Immigrants at Hong Kong Churches, Businesses, and Community 
+                            Organizations in Britain in 25 cities from Brighton to Glasgow</li>
+
+                            <li><b>April 18th</b>: Thomas, Jacob. “Affinity and Inequality in the Global Structure of the Visa-Free Mobility Network”,
+                            Presentation in Paper Session Migration Around the World at 2024 Population Association of America Annual Meeting, Columbus, Ohio, USA.</li>
+
+                            <li><b>March 27th</b>  Cambridge University Press offers a book contract from for my  manuscript Denial, Deterrence, and 
+                            Disenchantment: Why Many Never Immigrate</li>
+
+                            <li><b>February 5th</b>: Symbolic Interaction accepts for article publication my paper "Visual Art as a Channel and 
+                            Embodiment of Symbolic Interaction Between Migrants and the Native-born"</li>
+
+                            <li><b>February 5th</b>: International Migration Review accepts for article publication my paper 
+                            "How Family Ties Deter International Migration and Mobility"</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>2023</h2>
+                        <ul>
+                            <li><b>December 20th</b>:  Launch of Hong Kong Emigration Project survey for analytical sample</li>
+
+                            <li><b>December 11th</b>: “Not Settled Enough to Migrate: How the (Im)mobility Paradox Prevents Migration Across the 
+                            Life Course”, Presentation at the Xian Jiaotong University-Chinese University of Hong Kong Joint Conference. Hong Kong, China</li>
+
+                            <li><b>December 5th to 10th</b>: Field Research trip to Taiwan to recruit Hong Kong Immigrants</li>
+
+                            <li><b>October 20th</b>: I present 'Impact of an 'Emmet Till moment" versus a “Good Guy with a Gun” on Gun Control Opinions in the United States' in 
+                            Inequality Cluster of CUHK Population Working Group </li>
+
+                            <li><b>October 1st to December 5th</b>: I serve as coordinator of the Inequality Cluster for the Population
+                             Working Group at the Chinese University of Hong Kong</li>
+
+                            <li><b>October 16th</b>: Invited Presentation “What Stops People From Migrating and Traveling Abroad?”  in Yuying's undergraduate course 
+                            Migration and Policy Reflection on Global Citizenship</li>
+
+                            <li><b>September 1st</b>: Presented “From “Illegal” to “Undocumented”—Anti-Immigrant Reactance Toward a Ban of a Term Among
+                            Media Producers and Consumers”, Paper Session, Race, Ethnicity, and Politics, at 2023 Annual Meeting of American 
+                            Political Science Association, Los Angeles, USA. </li>
+
+                            <li><b>September 3rd</b>: Presented ( with coauthor Peng Huang), “Affinity and Inequality in the Global Structure of the Visa-Free Mobility 
+                            Network”  at Paper Session Globalization at 2023 Annual Meeting of American Political Science Association of America, Los Angeles, USA. </li>
+
+                            <li><b>September 7th - December 1st</b>: Course on Sociology and Modernity in the Age of Generative Artificial Intelligence taught to 
+                            Masters of Arts students at the Chinese University of Hong Kong</li>
+
+                            <li><b>August 23rd</b>: European Journal of Sociology accepts for article publication my paper "Disenchanted With the Immigrant Dream: 
+                            The Sociological Formation of Ex-Immigrant Subjectivity"</li>
+
+                            <li><b>August 20th</b>: Presented (with Peng Huang) “Affinity and Inequality in the Global Structure of the Visa-Free Mobility Network” in Paper Session Globalization at 2023 American Sociological
+                            Association of America, Philadelphia, USA. </li>
+
+                            <li><b>August 18th</b>: “Does the Race of Rapists and Rape Victims Matter for Opinions About Abortion? Anti-Abortion
+                            Universalism Vs. Latent Racial Eugenicist Bias in Attitudes Toward Abortion”, Paper
+                            Session Timely Topics at 2023 Annual Meeting of American Sociological Association of America, Philadelphia, USA. </li>
+
+                            <li><b>August 17th</b>: Presented “The Soft Power Cost of COVID-19: A Lose-Lose Outcome for China and the United States”,
+                            in Paper Session at 2023 Annual Meeting of International Chinese Sociological Association, Philadelphia, USA. August 2023</li>
+
+                            <li><b>June 26th</b>: Presented “Bureaucratic and Organizational Amenability to Racial Diversification: How Points 
+                            Systems Replaced White-Only Immigration Policies”, at Current Research on Comparative Sociology, 2023 International 
+                            ociological Association Global Congress, Melbourne, Australia.</li>
+
+                            <li><b>June 30th</b>: Research Grant Agency awards me a General Research Fund grant ($67,600USD) for my project "Who Realizes Their
+                            Online Intentions To Emigrate? What Distinguishes Emigrants from Potential Emigrants in the Context of Rapid and Sudden Social Change"</li>
+
+                            <li><b>May 31st</b>: Direct Grant (6,500 USD) Awarded for 'Impact of an 'Emmet Till moment" versus a “Good Guy with a Gun” on Gun Control 
+                            Opinions in the United States' </li>
+
+                            <li><b>April 15th</b>: Peng presents our coauthored paper “Affinity and Inequality in the Global Structure of the 
+                            Visa-Free Mobility Network” (presented by coauthor Peng Huang), Poster Presentation on Migration at 2023 Annual Meeting of 
+                            Population Association ofAmerica, New Orleans, USA. </li>
+
+                            <li><b>April 15th</b>: I present a poster for paper “What Types of Famous Political Dissidents Succeed in Emigrating From Autocratic States?: An Individual-Level Analysis of Emigration Control in the 
+                            People’s Republic of China”, Poster Presentation at 2023 Annual Meeting of Population Association of America, New Orleans, USA. </li>
+
+                            <li><b>April 13th</b>: “Why Do Some Emigrate and Most Merely “Want” To Emigrate? The Specific Events and General 
+                            Reasons Prompt Thoughts of Emigrating and Emigration”, Presentation in Paper Session 21st Century
+                            Migration Theory, 2023 Annual Meeting of Population Association of America, New Orleans, USA</li>
+
+                            <li><b>January 13th</b>: International Journal of Sociology accepts for publication my paper "Bureaucratic and Organizational Amenability to Racial Diversification:
+                            How Points Systems Replaced White-Only Immigration Policies"</li>
+
+                            <li><b>January 1st - May 1st</b>: I teach Qualitative Research Methods for the Chinese University of Hong Kong's 
+                            Sociology Department</li>
+
+                            
+
+
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>2022</h2>
+                        <ul>
+                            <li><b>December 5th</b>: I present "Disenchanted With the Immigration Dream: The Sociological Formation of Ex-Immigrant
+                            Subjectivity”, at the 2022 Chinese Sociological Association Annual Meeting, Guangzhou, China. (Online)</li>
+
+                            <li><b>July 20th - August 10th</b>: Conducted archival research for "Bureaucratic and Organizational Amenability Towards 
+                            Racial Diversification" at the Australian National library and  National Archives of Australia in Canberra</li>
+
+                            <li><b>July 15th</b>: Presented (with  Peng Huang ) “How Affinity and Hierarchy Structure the Global Network of Visa Free 
+                            Mobility, 1969- 2010” at the  2022 Sunbelt Annual Meeting, Cairns, Australia.</li>
+
+                            <li><b>May 31st </b>: Direct Grant (6,500 USD) Awarded for "Who Realizes Their Online Intentions To Emigrate? What Distinguishes Emigrants from 
+                            Potential Emigrants in the Context of Rapid and Sudden Social Change”</li>
+
+                            <li><b>January 15th</b>: Publication of first non-English book chapter: Thomas, Jacob, Lemeng Liang, Shigeto Sonoda, and Yu Xie. 2022. 
+                            “Shingata Korona wuirusu wa sekaino taichu/taibei ninsiki wo ikani kaetaka?” (How Did COVID-19 Change Global Views of China and US?) in 
+                            Shigeto Sonoda and Yu Xie, eds., Sekai no Taichu Ninshiki: Deta de saguru sono tokucho to henka (Global Views of China: Empirical Analysis of 
+                            Their Trends): Tokyo: University of Tokyo Press.</li>
+
+                            <li><b>January 1st - May 1st</b>: I teach Qualitative Research Methods (hybrid) for the Chinese 
+                            University of Hong Kong's Sociology Department</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>2021</h2>
+                        <ul>
+                            <li><b>December 14th</b>: I present  "Why Does the Chinese Government Allow Some Of Its Citizens To Leave or
+                            Return But Not Others?: The Extra-legal Logic of Exceptions to Chinese Laws of Exit and Entry”, 2021 Hong Kong 
+                            Sociological Association’s 2021 Annual Meeting</li>
+
+                            <li><b>October 10th</b>: I present “What Ties Individuals to a Society? How Mobility-Stability Capital Stratifies 
+                            International Legal Mobility from Mainland China to the United States,” at American Political Science Association's 
+                            2021 Annual Meeting (online) while in a 3-week hotel quarantine in Hong Kong</li>
+                            
+                            <li><b>August 7th</b>: Presided over session I organized on COVID-19 and Global Mobility at  2021 American Sociological
+                             Association Annual Meeting, Chicago, USA. (Online)</li>
+
+                            <li><b>August 9th</b>: I present “How Assimilation(ism) May Harm ‘Us’ As Well As ‘Them’: Conceptualizing Nationalist Chauvin(ism) as Variants of A 
+                            Societal-Level Auto-Immune Disorder,” at 2021 American Sociological Association Annual Meeting, Chicago, USA. (Online)</li>
+
+                            <li><b>July 31st</b>: I present “The Relationship Between International Visa-Free Mobility and Nation-State Homophily and
+                            Hierarchy, 1969- 2010,” at Polnet Conference, Pennsylvania State University, USA. </li>
+
+                            <li><b>June 26th</b>: I receive offer letter for Research Assistant Professor position at the Chinese University of Hong Kong</li>
+
+                            <li><b>January 15th</b>: Publication of first book chapter as Thomas, Jacob and Min Zhou. 2021. “Ethnic Entrepreneurship and Its Transnational Linkages.” Pp. 404-419 in Brenda S. A. Yeo
+                             and Francis Collins (eds.), Handbook on Transnationalism. Cheltenham, UK: Edward Elgar Publishing.</li>
+
+                            
+                        </ul>
+                    </div>
+                    <div>
+                        <h2>2020</h2>
+                        <ul>
+                            <li><b>December 10th</b>: I present book chapter coauthored with Min Zhou on “Ethnic Entrepreneurship 
+                            and Its Transnational Linkages,” at the Global Chinese Entrepreneurship
+                            Conference, UCLA Asia Pacific Institute, Los Angeles, USA. December 2020.</li>
+
+                            <li><b>July 21st</b>: November 28th, 2020: Presented paper “Why Does China Sometimes Allows Some of Its Citizens to Leave Its Territory But Not Others,” at International Chinese 
+                            Sociological Association, New York University-Shanghai, Online</li>
+
+                            <li><b>September 3rd</b>: Presented “How Have Xenophobic Politics and Policies Made the United States Less Attractive? How Unintended Chilling Impacts of Targeted Anti-Mobility Policies Extend Far Beyond Their Targets,” at  American Political Science Association Annual Meeting, 
+                            Citizenship and Migration Section: Election Cycles and Immigration, San Francisco, CA, USA, September 2020</li>
+
+                            <li><b>August 27th</b>: Presented “Strengths and Challenges of Cross-Cultural Field Research”, at American Sociological Association Annual Meeting, Methodology 
+                            Section: Investigating Qualitative Methods, San Francisco, USA. August 2020</li>
+
+                            <li><b>July 21st</b>: Publication of "From local control to remote control: an excavation of international mobility constraints" 
+                            online in Theory and Society</li>
+
+                            <li><b>April</b>: First in print publication-"When Political Freedom Does Not Offer Travel Freedom: The Varying Determinants of Visa-Free Travel 
+                            Opportunities" in International Migration</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
     </div>
     </body>
 </template>
 <style scoped>
+    .news-content{
+        margin-bottom: 400px;
+    }
+    h2{
+        margin-bottom: 30px;
+        font-weight: bold;
+    }
+    li{
+        margin-bottom: 10px;
+    }
     .about-me-intro > p{
         margin-top:50px;
     }
