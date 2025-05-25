760562046d9d078c627f9857865a7cec33d17b50
t12679 <32949438+t12679@users.noreply.github.com>
2025-05-25 09:08:05 +0200

    commit

--- src/Outline.vue
+++ src/Outline.vue
@@ -62,13 +62,13 @@ watch(
       <footer class="footer">
         <div class="wrapper footer-content">
           <div>
-            Department of Sociology, The Chinese University of Hong Kong<br/>
-            Email: <a href="mailto:j.thomas@cuhk.edu.hk">j.thomas@cuhk.edu.hk</a><br/>
+            Department of Sociology, Corvinus University of Budapest<br/>
+            Email: <a href="mailto:jrt@uni-corvinus.hu">jrt@uni-corvinus.hu</a><br/>
           </div>
           <div class="links">
             <ul class="flex-container">
                 <li>
-                    <a href="mailto:j.thomas@cuhk.edu.hk">
+                    <a href="mailto:jrt@uni-corvinus.hu">
                         <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                             <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                         </svg>
