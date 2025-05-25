696999b650597f86f30d9a29df42fde1f99d6be6
t12679 <32949438+t12679@users.noreply.github.com>
2025-05-25 08:24:48 +0200

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -45,14 +45,14 @@
                     <div class="position">
                         <h3>Jacob Richard Thomas(唐龙)</h3>
                         <h4>Research Assistant Professor</h4>
-                        <h5>The Chinese University of Hong Kong</h5>
+                        <h5>Corvinus University of Budapest</h5>
                         <h5>Department of Sociology</h5>
                         <h4>PhD (UCLA)</h4>
                     </div>
                     <div class="links">
                         <ul class="flex-container">
                             <li>
-                                <a href="mailto:j.thomas@cuhk.edu.hk">
+                                <a href="mailto:jrt@uni-corvinus.hu">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                                         <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
                                     </svg>
